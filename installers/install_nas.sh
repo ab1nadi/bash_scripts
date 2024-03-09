6645f1b0f8135cafd08195227eead560ad23f5aa
Abinadi Swapp <abinadiswapp@gmail.com>
2024-03-09 00:23:22 -0700

    It is mostly working, cockpit still wants to mount it permanently but that will break things. Gonna have to come up with something so it doesn't think so

--- installers/install_nas.sh
+++ installers/install_nas.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Script Name: install_cockpit.sh
 #
@@ -22,10 +22,34 @@ function setVar() {
     local value="$3"
     
     # Use awk to update the variable value in a temporary file
-    awk -v var="$variable" -v val="$value" 'BEGIN { FS = "=" } $1 == var {print var "=" val; next} {print $0}' "$file" > "$file.tmp"
+    awk -v var="$variable" -v val="$value" 'BEGIN { FS = "=" } $1 == var {print var "=" "\"" val "\""; next} {print $0}' "$file" > "$file.tmp"
+
+  
+    # Replace the original file with the updated content
+    mv "$file.tmp" "$file"
+
+    # so that permission don't keep changing
+    chmod 777 $file
+}
+
+# sets a variable array
+# in another file
+function setVarArray() {
+    local file="$1"
+    local variable="$2"
+    local values=("${@:3}")
+
+    # Serialize the array into a string (comma-separated values)
+    local serialized_values="($(printf ' "%s"' "${values[@]}"))"  
+
+    # Use awk to update the variable value in a temporary file
+    awk -v var="$variable" -v val="$serialized_values" 'BEGIN { FS = "=" } $1 == var {print var "=" val; next} {print $0}' "$file" > "$file.tmp"
 
     # Replace the original file with the updated content
     mv "$file.tmp" "$file"
+
+    # Set permissions (adjust as needed) u=rwx for when this is in production
+    chmod 777 "$file"
 }
 
 # backups a file 
@@ -36,51 +60,79 @@ function backup() {
 }
 
 # variables
-iscsiconf="/etc/iscsi/iscsid.conf"
+ad_dir="/usr/local/bin"
+cron="@reboot $ad_dir/connect_nas.sh"
 ip="10.29.254.19"
-mount="/mnt/iscsi"
-targetname=""
-target=""
+mounts=()
+targetnames=()
+targets=()
 
-# parse some variables passed as args
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    -ip)
-      ip="$2"
-      shift 2
-      ;;
-    -mt)
-      mount="$2"
-      shift 2
-      ;;
-    *)
-      targetname="$1"
-      shift
-      ;;
+while getopts ":i:m:t:" opt; do
+  case $opt in
+    i) ip="$OPTARG";;
+    m) mounts+=("$OPTARG");;
+    t) targetnames+=("$OPTARG");;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+    :) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
   esac
 done
 
-# The target name of the zvolume
-if [ -z "$targetname" ]; then
-  echo "Please provide a NAS target name."
-  exit 0
-fi
-
-# create mount directory if it doesnt already exist
-if test -d /path/to/directory; then
-  echo "Mount point already exists."
-else 
-    mkdir $mount
-    echo "Created mount point."
-fi
+# Print the values for demonstration purposes
 
 
-target=$(iscsiadm -m discovery -t sendtargets -p "$ip" | awk -v targ="$targetname" '$2 ~ targ {print $2}')
+echo "Creating mount points:"
+for mount in "${mounts[@]}"; do
+  if test -d $mount; then
+    echo -e "$mount \t=> already exists."
+  else 
+      mkdir $mount
+      echo -e "$mount /t=> created."
+  fi
+done
+
+
+echo "Getting targets:"
+# iterate over given target names 
+for name in "${targetnames[@]}"; do
+    # Perform iSCSI discovery and filter targets using awk
+    discovered_targets=($(iscsiadm -m discovery -t sendtargets -p "$ip" | awk -v targ="$name" '$2 ~ targ {print $2}'))
+
+    # Check if any targets were discovered and append to the targets array
+    if [ ${#discovered_targets[@]} -gt 0 ]; then
+        targets+=("${discovered_targets[0]}")
+
+        echo -e "$name \t=> ${discovered_targets[0]} "
+    fi
+done
 
 
 
 # set the variables in the connect_nas script
 setVar ../helper_scripts/connect_nas.sh ip $ip
-setVar ../helper_scripts/connect_nas.sh target $target
-setVar ../helper_scripts/connect_nas.sh mount $mount
+setVarArray ../helper_scripts/connect_nas.sh mounts "${mounts[@]}"
+setVarArray ../helper_scripts/connect_nas.sh targets "${targets[@]}"
 
+
+
+# run the nas
+echo "Connecting nas for the first time!"
+
+# run the connect nas script 
+./../helper_scripts/connect_nas.sh
+
+
+echo "Putting script for cron in /usr/local/bin:"
+# copy the connect nas script into /usr/local/bin
+# then it can be run as a cron job
+cp ../helper_scripts/connect_nas.sh "$ad_dir/connect_nas.sh"
+
+
+echo "Adding script to crontab:" 
+# Check if the cron entry already exists
+if ! (crontab -l | grep -q "$cron"); then
+    # If it doesn't exist, echo the new command and pipe it to crontab for the root user
+    echo "$cron" | crontab -
+    echo "Cron entry added successfully."
+else
+    echo "Cron entry already exists."
+fi
